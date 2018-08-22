0a13d13e75bb00054aea8a0cebddba5acb4cc1e7
Tim Campbell <tim.campbell@btinternet.com>
2018-08-22 19:13:02 +0100

    feat: added components directory, Map and SearchList components
    Added basic components for the project to ensure they are rendering
    in React.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchList from './components/SearchList';
+import Map from './components/Map';
 
 class App extends Component {
   render() {
@@ -10,9 +12,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchList />
+        <Map />
       </div>
     );
   }
